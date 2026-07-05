9d8695d425220e78bb7e3b7d185743d99bf60c5e
numanluau <numantf3@gmail.com>
2026-07-05 08:55:19 +0500

    Update site

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/')
+def index():
+    return send_from_directory(BASE_DIR, 'index.html')
+
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory(BASE_DIR, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
